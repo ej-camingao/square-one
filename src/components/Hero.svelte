acb7a67bd820ae8593362d5c99026d166cd51aa9
Ethan Camingao <ethancamingao@gmail.com>
2024-08-15 00:45:37 +0800

    Initial Commit

---
+++ src/components/Hero.svelte
@@ -0,0 +1,36 @@
+<script>
+    import CtAs from "./CTAs.svelte";
+    import Header from "./Header.svelte";
+    import SectionWrapper from "./SectionWrapper.svelte";
+
+</script>
+<SectionWrapper>
+    <Header/>
+    <div
+    class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14"
+>
+    <h2 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold">
+        Square One <span class="text-green-700">Builders</span>
+        <br/>
+        Just Got a <span class="text-green-700">Wesbite</span>
+    </h2>
+    <p class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] mx-auto w-full">
+    Less thinking, <span class="italic">more building</span>
+    </p>
+    <CtAs/>
+    <div class="flex items-center justify-center gap-2 text-base">
+        <p>4.6</p>
+        {#each [0,1,2,3,4,5] as index }
+            <div class="grid place-items-center relative">
+                <i class="fa-solid fa-star opacity-0"></i>
+                <div class={"absolute top-0 left-0 grid place-items-center" + (index==4 ? "width [60%] overflow-hidden" : " ")}>
+                    <i class="fa-solid fa-star text-amber-400"></i>
+                </div>
+            </div>
+        {/each}
+        <p>500+</p>
+    </div>
+    </div>
+
+
+</SectionWrapper>
---
+++ src/components/Product.svelte
@@ -0,0 +1,53 @@
+<script>
+    import ProductCard from "./ProductCard.svelte";
+import SectionWrapper from "./SectionWrapper.svelte";
+    let productFeatures = [
+        {
+            featureList: [
+                "Daily Checks",
+                "Weighted Regression",
+            ],
+            description: 'Square One Builders is grounded in Integrity',
+            imgUrl: 'assets/selection.png'
+        },
+        {
+            featureList: [
+                "Daily Checks",
+                "Weighted Regression",
+            ],
+            description: 'Square One Builders is grounded in Truth',
+            imgUrl: 'assets/training.png'
+        },
+        {
+            featureList: [
+                "Daily Wows",
+                "Beautiful Builds",
+            ],
+            description: 'Square One Builders is grounded in Love',
+            imgUrl: 'assets/goal.png'
+        }
+    
+    ]
+
+</script>
+<SectionWrapper id="product">
+    <div class="flex flex-col gap-10 sm:gap-14 md:gap-24 flex-1 items-center justify-center pb-10 md:pb-14">
+        <div class="flex flex-col gap-2">
+            <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">Build Your 
+                <span class="text-green-700"> Foundation</span>
+            </p>
+            <h3 class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center">Features For Everyone</h3>
+                {#each productFeatures as productFeature, index}
+                    <ProductCard productFeature={productFeature} index={index}>
+                        {#if index===0}
+                        Experience a beginner exercise <span class="text-indigo-400 font-medium">master</span> class
+                        {:else if index===1}
+                        Lay the foundation for <span class="text-indigo-400 font-medium"> routine</span> and consistency
+                        {:else}
+                        Workouts specific to your <span class="text-indigo-400 font-medium">environment</span> and <span class="text-indigo-400 font-medium">expertise</span>
+                        {/if}
+                    </ProductCard>
+                {/each}
+        </div>
+    </div>
+</SectionWrapper>
